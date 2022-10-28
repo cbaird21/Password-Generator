bc865650f7407248fb08841e8bd8473fb05b242c
unknown <chandler.baird21@gmail.com>
2022-10-27 20:08:32 -0600

    done for this evening still need to better comment out CSS and expand PSUEDO

--- script.js
+++ script.js
@@ -1,61 +1,136 @@
 // Assignment code here
-// list out the criteria options we must select yes or no to in Alert
-var upperCase= ["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" Y" "Z"]
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+// all arrays
+var lowerCaseArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
+var upperCaseArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specialArr = ["!", "@", "#", "$", "*", "%"];
 // This "confirm alert" prompts user on page to select the different criterias
-// alert("Please select which criteria you'd like in your password.");4 loop for length of password
-var characterLength = prompt(
-  "select your character length between 8 and 128 characters"
-);
-console.log(characterLength);
+//create function
+function generatePassword() {
+  var passwordLength = prompt(
+    "Chose a length of your password between 8 and 128 characters."
+  );
+  passwordLength = Number(passwordLength);
+  console.log(passwordLength);
 
-var upperCase = confirm("Select ok if you'd like uppercases in your password.");
-console.log(upperCase);
+  var upperCase = confirm(
+    "Please confirm if you'd like uppercases in your password."
+  );
+  console.log(upperCase);
 
-var lowerCase = confirm(
-  "Select ok if you'd like lower cases in your password."
-);
-console.log(lowerCase);
+  var lowerCase = confirm(
+    "Please confirm if you'd like lower cases in your password."
+  );
+  console.log(lowerCase);
 
-var numberCharacters = confirm(
-  "Select ok if you'd like numbers in your password."
-);
-console.log(numberCharacters);
+  var numbers = confirm(
+    "please confirm if you'd like numbers in your password."
+  );
+  console.log(numbers);
 
-var specialCharacters = confirm(
-  "Select ok if you'd like special characters in your password."
-);
-console.log(specialCharacters);
+  var special = confirm(
+    "pleaseconfirm if you'd like special characters in your password."
+  );
+  console.log(special);
 
-// Need to track to ensure 1 or more criteria is accepted. and operator &&
-//
-// if (var&&var&&var&&)
-var newChars =[]
+  // Need to track to ensure 1 or more criteria is accepted. and operator &&
+  //
+  // if (var&&var&&var&&)
+  var newChars = [];
 
-if(upperCase){
-  newChars= newChars.concat"upperCaseArr";
-};
-console.log(newChars);
+  if (upperCase) {
+    newChars = newChars.concat(upperCaseArr);
+  }
+  console.log(newChars);
 
-if(lowerCase){
-  newChars= newChars.concat"lowerCase";
-};
+  if (lowerCase) {
+    newChars = newChars.concat(lowerCaseArr);
+  }
 
-if(numberCharacters{
-  newChars= newChars.concat"numberCharacters";
-};
+  if (numbers) {
+    newChars = newChars.concat(numbersArr);
+  }
 
-if(specialCharacters){
-  newChars= newChars.concat"specialCharacters";
-};
+  if (special) {
+    newChars = newChars.concat(specialArr);
+  }
 
-console.log(newChars);
+  console.log(newChars);
 
-// create final password:
-var finalPassword = []
+  // create final password:
+  var finalPassword = [];
 
-// create random password for loop
-
+  // create random password for loop
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * newChars.length);
+    finalPassword = finalPassword.concat(newChars[randomNumber]);
+    console.log(finalPassword);
+  }
+  // omits ray elements seperated by periods is what join does
+  finalPassword = finalPassword.join("");
+  console.log(newChars);
+  console.log(finalPassword);
+  return finalPassword;
+}
 
 // return:keeps other code from being done
 // alert select one criteria before generating password.
